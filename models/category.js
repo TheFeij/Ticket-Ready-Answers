bd67f1e01a994f3e81c27d9368c196c6a1f5fd7d
Abolfazl Moradi Feijani <abolfazl.moradi.feijani@gmail.com>
2023-07-31 09:03:24 +0330

    Added functions to get the full hierarchy of categories in the database

--- models/category.js
+++ models/category.js
@@ -45,7 +45,72 @@ function validateCategory(category){
     return schema.validate(category)
 }
 
+/**
+ * Recursively constructs the full tree of categories starting from the specified category ID.
+ *
+ * @param {string} categoryId - The ID of the category to start building the tree from.
+ * @returns {Promise<Object>} A promise that resolves to an object representing the category and its subcategories
+ *                            The object has the following properties:
+ *                            - id: The ID of the category.
+ *                            - name: The name of the category.
+ *                            - children: An array of nested category objects (subcategories).
+ * @throws {Error} If there's an issue with the database query or connection.
+ */
+async function getFullTree(categoryId) {
+    // Find the category with the provided categoryId in the database.
+    const category = await Category.findById(categoryId)
+
+    // If the category doesn't exist, return null as there's no tree to build.
+    if (!category) {
+        return null
+    }
+
+    // Find all subcategories of the current category.
+    const subCategories = await Category.find({ parent: categoryId })
+
+    // Recursively construct the tree for each subcategory.
+    const subCategoryTree = await Promise.all(
+        subCategories.map((subCategory) => getFullTree(subCategory._id))
+    )
+
+    // Construct the object representing the category and its children (subcategories).
+    return {
+        id: category._id,
+        name: category.name,
+        children: subCategoryTree,
+    }
+}
+
+/**
+ * Retrieves all root categories (categotries with no parent) from the database.
+ *
+ * @returns {Promise<Array>} A promise that resolves to an array of root categories.
+ * @throws {Error} If there's an issue with the database query or connection.
+ */
+async function getRoots() {
+    return Category.find({ parent: null })
+}
+
+/**
+ * Retrieves all categories and constructs their full hierarchical tree.
+ *
+ * @returns {Promise<Array>} A promise that resolves to an array representing the full tree of categories.
+ *                          Each element in the array represents a root category and its nested subcategories.
+ * @throws {Error} If there's an issue with the database query or connection.
+ */
+async function getAllCategories(){
+    const rootCategories = await getRoots()
+    if (rootCategories.length === 0) {
+        return []
+    } else {
+        return await Promise.all(
+            rootCategories.map((rootCategory) => getFullTree(rootCategory._id))
+        )
+    }
+}
+
 
 
 module.exports.Category = Category
 module.exports.validateCategory = validateCategory
+module.exports.getAllCategories = getAllCategories
